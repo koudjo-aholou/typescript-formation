a22f465f23c40873260670811d7045e3d9ac6738
Koudjo AHOLOU <ah.koudjo@gmail.com>
2024-04-24 01:01:22 +0200

    transpilation

---
+++ initTypescript/index.js
@@ -0,0 +1,9 @@
+function greeter2(person) {
+    return "Hello, " + person;
+}
+// Déclaration d'une variable avec typage explicite
+var message = "Bonjour TypeScript !";
+// Fonction avec typage explicite des paramètres et du retour
+function add(x, y) {
+    return x + y;
+}
--- initTypescript/index.ts
+++ initTypescript/index.ts
@@ -1,3 +1,11 @@
-function greeter(person) {
+function greeter23(person) {
   return "Hello, " + person;
+}
+
+// Déclaration d'une variable avec typage explicite
+let message6: string = "Bonjour TypeScript !";
+
+// Fonction avec typage explicite des paramètres et du retour
+function add6(x: number, y: number): number {
+    return x + y;
 }
