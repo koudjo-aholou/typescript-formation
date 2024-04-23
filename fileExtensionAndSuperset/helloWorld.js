fb6500f328077894b2efb6045633bbc1ab5daa31
Koudjo AHOLOU <ah.koudjo@gmail.com>
2024-04-24 00:29:28 +0200

    extensions

---
+++ fileExtensionAndSuperset/helloWorld.js
@@ -0,0 +1,9 @@
+const boutiquier = "Yasser".toUppercase();
+const whatHappened = boutiquier.toUppercase();
+const ticketDeCaisse = { title: "", price: undefined, imgPath: "" }
+const imgPath = ticketDeCaisse.img;
+
+const backEndContract = { ...ticketDeCaisse, quantite: 100 }
+const whyNot = null + 11;
+const whyNot2 = [] + 11
+
---
+++ fileExtensionAndSuperset/helloWorld.ts
@@ -0,0 +1,12 @@
+const boutiquier2: string = "Yasser"
+const boutiquier1 = "Jonathan joestar"
+const whatHappenedHere = boutiquier1.toUppercase();
+
+interface BackEndContract { title: string, price: number, imgPath: string, quantity: number }
+
+
+const whyNot0 = null + 11;
+const whyNot1 = [] + 11;
+
+const tableau = ["1", "2"]
+const itab = tableau[2].toUpperCase()
