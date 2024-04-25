c84fae1da91c5ace8ce031985f6e61b0e0366b08
Koudjo AHOLOU <ah.koudjo@gmail.com>
2024-04-26 00:05:26 +0200

    class

---
+++ initTypescriptConfig/src/classe/Manga.ts
@@ -0,0 +1,33 @@
+export class Manga {
+  titre: string;
+  auteur: string;
+  annee_publication: number;
+  genre: string[];
+  volumes: number;
+
+  constructor(
+    titre: string,
+    auteur: string,
+    annee_publication: number,
+    genre: string[],
+    volumes: number,
+  ) {
+    this.titre = titre;
+    this.auteur = auteur;
+    this.annee_publication = annee_publication;
+    this.genre = genre;
+    this.volumes = volumes;
+  }
+  getTitle(): string {
+    return this.titre;
+  }
+}
+
+// Exemple d'utilisation de la classe
+const livre1 = new Manga(
+  'Naruto',
+  'Masashi Kishimoto',
+  1999,
+  ['Action', 'Aventure', 'Ninja'],
+  72,
+);
