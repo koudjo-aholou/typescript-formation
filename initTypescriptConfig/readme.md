f7d7d030a8612cfe87108f18a53eb5664fd8cf39
Koudjo AHOLOU <ah.koudjo@gmail.com>
2024-04-25 13:05:51 +0200

    add mock data

---
+++ initTypescriptConfig/readme.md
@@ -0,0 +1,1 @@
+##Type data, method,variable
---
+++ initTypescriptConfig/src/utils/mock/mockMangaAnime.js
@@ -0,0 +1,88 @@
+export const MangaAnime = {
+  anime: [
+    {
+      titre: 'Naruto',
+      isAnime: true,
+    },
+    {
+      titre: 'Bleach',
+      isAnime: true,
+    },
+    {
+      titre: 'One Piece',
+      isAnime: true,
+    },
+    {
+      titre: 'Death Note',
+      isAnime: true,
+    },
+    {
+      titre: 'Dragon Ball',
+      isAnime: true,
+    },
+    {
+      titre: 'Attack on Titan',
+      isAnime: true,
+    },
+    {
+      titre: 'Fullmetal Alchemist',
+      isAnime: true,
+    },
+    {
+      titre: 'My Hero Academia',
+      isAnime: true,
+    },
+    {
+      titre: 'Tokyo Ghoul',
+      isAnime: true,
+    },
+    {
+      titre: 'Demon Slayer: Kimetsu no Yaiba',
+      isAnime: true,
+    },
+    {
+      titre: 'Hunter x Hunter',
+      isAnime: true,
+    },
+    {
+      titre: 'One Punch Man',
+      isAnime: true,
+    },
+    {
+      titre: 'Fairy Tail',
+      isAnime: true,
+    },
+    {
+      titre: 'Black Clover',
+      isAnime: true,
+    },
+    {
+      titre: 'Haikyu!!',
+      isAnime: true,
+    },
+    {
+      titre: 'JoJo s Bizarre Adventure',
+      isAnime: true,
+    },
+    {
+      titre: 'Sword Art Online',
+      isAnime: true,
+    },
+    {
+      titre: 'The Promised Neverland',
+      isAnime: true,
+    },
+    {
+      titre: 'Shingeki no Kyojin: Before the Fall',
+      isAnime: false,
+    },
+    {
+      titre: 'Dragon Ball Super',
+      isAnime: true,
+    },
+    {
+      titre: 'Gantz',
+      isAnime: true,
+    },
+  ],
+};
