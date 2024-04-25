3233c6362c3a517bd2a035ed54c5215c7f74c2af
Koudjo AHOLOU <ah.koudjo@gmail.com>
2024-04-25 12:52:24 +0200

    folder

---
+++ initTypescriptConfig/info/tools.ts
@@ -0,0 +1,12 @@
+// Typescript error
+
+// Limit the use of any Type : Tyoescript s type system is gradual (add  types little by little) and optionnal because you can disable the type checker
+// strick null check
+
+// const y: number | null = null;
+
+// let yo: number = undefined;
+// implicit any
+// const yy = (a, b) => a + b;
+
+// const notAny: any = 'y';
