a322a3c43b0b5db8d74f8cbc7ade8fc1ab56be99
Koudjo AHOLOU <ah.koudjo@gmail.com>
2024-04-25 09:59:16 +0200

    formation tsconfig

---
+++ initTypescriptConfig/src/utils/tools.ts
@@ -0,0 +1,8 @@
+//Typescript error
+
+// Limit the use of any Type : Tyoescript s type system is gradual (add  types little by little) and optionnal because you can disable the type checker
+// strick null check
+const y: number = null;
+
+// implicit any
+const yy = (a, b) => a + b;
